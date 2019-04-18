c6b6b80b4f2049e704a9ba5881257ed0f75130a1
DUSART Jeremie <jeremie.dusart@inria.fr>
2019-04-18 11:56:39 +0200

    Error message in alert

--- RDFConverter/src/main/java/fr/inria/RDFConverter/FormController.java
+++ RDFConverter/src/main/java/fr/inria/RDFConverter/FormController.java
@@ -18,11 +18,10 @@ import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.shape.Path;
 
 public class FormController {
 	private App master;
-	
+
 	public static final List<RDFFormat> RDFFormats = Arrays.asList(new RDFFormat[] {
 			RDFFormat.BINARY,
 			RDFFormat.JSONLD,
@@ -35,30 +34,30 @@ public class FormController {
 			RDFFormat.TRIG,
 			RDFFormat.TURTLE
 	});
-	
+
 	@FXML
 	private TextField  inputFile;
 	@FXML
 	private ComboBox<RDFFormat> inputFileFormat;
 	@FXML
 	private ComboBox<RDFFormat> outputFileFormat;
-	
+
 	public void initialize() {
 		inputFileFormat.getItems().addAll(RDFFormats);
 		outputFileFormat.getItems().addAll(RDFFormats);
 
 	}
-	
+
 	public void selectInputFile() {
 		File res = master.createFileChooser("Select rdf file");
 		if (res!=null)
 			this.inputFile.setText(res.getAbsolutePath().toString());
 	}
-	
+
 	public void close() throws Exception {
 		this.master.stop();
 	}
-	
+
 	public void convert() {
 		System.out.println("Convert "+this.inputFile.getText());
 		System.out.println("From format "+inputFileFormat.getSelectionModel().getSelectedItem());
@@ -74,38 +73,45 @@ public class FormController {
 			master.createAlertError("Error", "Error", "No output file format.");
 			return ;
 		}
-		FileInputStream fin;
-		try {
-			File fp = Paths.get(this.inputFile.getText()).toFile();
-			if (!fp.exists()){
-				master.createAlertError("Error", "Error", "File does not exist.");
-				return ;
-			}
-			fin = new FileInputStream(fp);
-			Model model = Rio.parse(fin, "", inFormat);
-			File saveFile = master.createFileChooserSave("Select rdf file");
-			if (saveFile== null) {
-				master.createAlertError("Error", "Error", "No destination selected.");
-				return ;
-			}
-			FileOutputStream fout = new FileOutputStream(saveFile);
-			Rio.write(model, fout, outFormat);		
-			master.createAlertSuccess("Success", "Convertion", "Convertion done.");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (RDFParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedRDFormatException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		FileInputStream fin=null;
+		File fp = Paths.get(this.inputFile.getText()).toFile();
+		if (!fp.exists()){
+			master.createAlertError("Error", "Error", "File does not exist.");
+			return ;
 		}
+		try {
+			fin = new FileInputStream(fp);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			master.createAlertError("Error", "Error creating inputstream", e.getMessage());
+			return ;
+		}
+		Model model;
+		try {
+			model = Rio.parse(fin, "", inFormat);
+		} catch (RDFParseException | UnsupportedRDFormatException | IOException e) {
+			e.printStackTrace();
+			master.createAlertError("Error", "Error parsing input file", e.getMessage());
+			return ;
+		}
+		File saveFile = master.createFileChooserSave("Select rdf file");
+		if (saveFile== null) {
+			master.createAlertError("Error", "Error", "No destination selected.");
+			return ;
+		}
+		FileOutputStream fout;
+		try {
+			fout = new FileOutputStream(saveFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			master.createAlertError("Error", "Error creating outputstream", e.getMessage());
+			return ;
+		}
+		Rio.write(model, fout, outFormat);		
+		master.createAlertSuccess("Success", "Convertion", "Convertion done.");
+
 	}
-	
+
 	public void setMaster(App app) {
 		this.master = app;
 	}
